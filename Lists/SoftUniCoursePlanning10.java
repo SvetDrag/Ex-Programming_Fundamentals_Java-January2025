4608a07b4b9904694a8ff9b187faf93b797ce94c
SvetDrag <svetlozardragnev@gmail.com>
2025-02-09 22:29:25 +0200

    Ex10Finale

--- Lists/SoftUniCoursePlanning10.java
+++ Lists/SoftUniCoursePlanning10.java
@@ -9,42 +9,78 @@ import java.util.stream.Collectors;
 public class SoftUniCoursePlanning10 {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
         List<String> text = Arrays.stream(scanner.nextLine().split(", ")).collect(Collectors.toList());
 
         String command = scanner.nextLine();
 
-        while (!command.equals("course start")){
-            if (command.contains("Add:")){
-                String lessonToAdd = command.split(":")[1];
-                text.add(lessonToAdd);
-            }
-            else if (command.contains("Isert:")){
-                String lessonToInsert = command.split(":")[1];
-                int index = Integer.parseInt(command.split(":")[2]);
+        while (!command.equals("course start")) {
+            String[] tokens = command.split(":");
+            String action = tokens[0];
 
-                text.add(index, lessonToInsert);
-            }
-            else if (command.contains("Remove:")){
-                String lessonToRemove = command.split(":")[1];
-                text.remove(lessonToRemove);
-            }
-            else if (command.contains("Swap:")){
-                String lesson1 = command.split(":")[1];
-                String lesson2 = command.split(":")[2];
-                if (text.contains(lesson1) && text.contains(lesson2)){
-                    Collections.swap(text, text.indexOf(lesson1), text.indexOf(lesson2));
+            if (action.equals("Add")) {
+                String lessonToAdd = tokens[1];
+                if (!text.contains(lessonToAdd)) {
+                    text.add(lessonToAdd);
                 }
-                else {
-                    continue;
+            } else if (action.equals("Insert")) {
+                String lessonToInsert = tokens[1];
+                int index = Integer.parseInt(tokens[2]);
+
+                if (!text.contains(lessonToInsert) && index >= 0 && index <= text.size()) {
+                    text.add(index, lessonToInsert);
+                }
+            } else if (action.equals("Remove")) {
+                String lessonToRemove = tokens[1];
+                if (text.contains(lessonToRemove)) {
+                    int indexToRemove = text.indexOf(lessonToRemove);
+                    text.remove(lessonToRemove);
+
+
+                    if (indexToRemove < text.size() && text.get(indexToRemove).equals(lessonToRemove + "-Exercise")) {
+                        text.remove(indexToRemove);
+                    }
+                }
+            } else if (action.equals("Swap")) {
+                String lesson1 = tokens[1];
+                String lesson2 = tokens[2];
+
+                if (text.contains(lesson1) && text.contains(lesson2)) {
+                    int index1 = text.indexOf(lesson1);
+                    int index2 = text.indexOf(lesson2);
+
+                    Collections.swap(text, index1, index2);
+
+                    // Swap exercises if they exist
+                    if (text.contains(lesson1 + "-Exercise")) {
+                        text.remove(lesson1 + "-Exercise");
+                        text.add(index2 + 1, lesson1 + "-Exercise");
+                    }
+
+                    if (text.contains(lesson2 + "-Exercise")) {
+                        text.remove(lesson2 + "-Exercise");
+                        text.add(index1 + 1, lesson2 + "-Exercise");
+                    }
+                }
+            } else if (action.equals("Exercise")) {
+                String lessonTitle = tokens[1];
+
+                if (text.contains(lessonTitle)) {
+                    int lessonIndex = text.indexOf(lessonTitle);
+                    if (!text.contains(lessonTitle + "-Exercise")) {
+                        text.add(lessonIndex + 1, lessonTitle + "-Exercise");
+                    }
+                } else {
+                    text.add(lessonTitle);
+                    text.add(lessonTitle + "-Exercise");
                 }
             }
-            else if (command.contains("Exercise:")){
-                String exerciseName = command.split(":")[1];
-                if (text.contains(exerciseName)){
-                    text.add(text.indexOf(exerciseName), exerciseName + "-Exercise");
-                }
-            }
+
+            command = scanner.nextLine();
+        }
+
+        for (int i = 0; i < text.size(); i++) {
+            System.out.printf("%d.%s%n", i + 1, text.get(i));
         }
     }
 }
