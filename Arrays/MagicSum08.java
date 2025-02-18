3fc31d8329b1f7d6728404640ad0189addb6fa58
SvetDrag <svetlozardragnev@gmail.com>
2025-02-18 17:26:24 +0200

    Ex08

--- Arrays/MagicSum08.java
+++ Arrays/MagicSum08.java
@@ -7,12 +7,22 @@ public class MagicSum08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int [] num = Arrays.stream(scanner.nextLine().split(" "))
-                        .mapToInt(Integer::parseInt)
-                        .toArray();
 
-        int magicNum = Integer.parseInt(scanner.nextLine());
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
+
+        int targetSum = Integer.parseInt(scanner.nextLine());
+
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == targetSum) {
+                    System.out.println(numbers[i] + " " + numbers[j]);
+                }
+            }
+        }
 
 
     }
