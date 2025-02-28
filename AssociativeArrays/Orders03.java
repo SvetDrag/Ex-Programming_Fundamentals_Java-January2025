ce336c742451d9fdeec9ae0895958f3a44963536
SvetDrag <svetlozardragnev@gmail.com>
2025-02-28 19:53:15 +0200

    Ex2

--- AssociativeArrays/Orders03.java
+++ AssociativeArrays/Orders03.java
@@ -7,5 +7,24 @@ public class Orders03 {
 
         Scanner scanner = new Scanner (System.in);
 
+        String input = scanner.nextLine();
+
+        while (!input.equals("buy")){
+
+
+            String name = input.split(" ")[0];
+            double price = Double.parseDouble(input.split(" ")[1]);
+            double quantity = Double.parseDouble(input.split(" ")[2]);
+
+
+            
+
+
+
+
+
+            input = scanner.nextLine();
+        }
+
     }
 }
