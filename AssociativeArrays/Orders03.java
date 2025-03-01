c59205f5303b807bcb40d847ae764ae4c3ee5ba5
SvetDrag <svetlozardragnev@gmail.com>
2025-03-01 14:37:47 +0200

    Ex03

--- AssociativeArrays/Orders03.java
+++ AssociativeArrays/Orders03.java
@@ -1,30 +1,35 @@
 package AssociativeArrays;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Orders03 {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner (System.in);
+        Scanner scanner = new Scanner(System.in);
+        Map<String, Double> productQuantity = new LinkedHashMap<>();
+        Map<String, Double> productPrice = new LinkedHashMap<>();
 
         String input = scanner.nextLine();
 
-        while (!input.equals("buy")){
+        while (!input.equals("buy")) {
+            String[] data = input.split(" ");
+            String name = data[0];
+            double price = Double.parseDouble(data[1]);
+            double quantity = Double.parseDouble(data[2]);
 
+            productPrice.put(name, price);
 
-            String name = input.split(" ")[0];
-            double price = Double.parseDouble(input.split(" ")[1]);
-            double quantity = Double.parseDouble(input.split(" ")[2]);
-
-
-
-
-
-
-
+            productQuantity.put(name, productQuantity.getOrDefault(name, 0.0) + quantity); 
 
             input = scanner.nextLine();
         }
 
+        for (Map.Entry<String, Double> entry : productQuantity.entrySet()) {
+            String product = entry.getKey();
+            double totalPrice = productPrice.get(product) * entry.getValue();
+            System.out.printf("%s -> %.2f%n", product, totalPrice);
+        }
     }
 }
