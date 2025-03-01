eabbb5829ccfac7f97486c743c5efe0aa35b64f6
SvetDrag <svetlozardragnev@gmail.com>
2025-03-01 14:08:20 +0200

    Ex01

--- AssociativeArrays/CountCharsInAString01.java
+++ AssociativeArrays/CountCharsInAString01.java
@@ -1,8 +1,31 @@
 package AssociativeArrays;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class CountCharsInAString01 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner (System.in);
+
+        String text = scanner.nextLine();
+
+
+        Map<Character, Integer> charsCount = new LinkedHashMap<>();
+
+        for (char c : text.toCharArray()){
+
+            if (c != ' ') {
+                charsCount.put(c, charsCount.getOrDefault(c, 0) + 1);
+            }
+
+        }
+
+        for (Map.Entry<Character, Integer> entry : charsCount.entrySet()){
+            System.out.printf("%c -> %d%n", entry.getKey(), entry.getValue());
+        }
 
 
 
