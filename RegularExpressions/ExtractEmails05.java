c122c6e4d3073047c96783f7fb0812418de48f16
SvetDrag <svetlozardragnev@gmail.com>
2025-03-15 17:49:59 +0200

    Ex05

---
+++ RegularExpressions/ExtractEmails05.java
@@ -0,0 +1,12 @@
+package RegularExpressions;
+
+import java.util.Scanner;
+
+public class ExtractEmails05 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
